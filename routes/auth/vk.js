6cc726f512c3c1eddd11da3b9ef55cdfec76acbf
Roman <romashka.ross@gmail.com>
2021-06-10 01:10:55 +0700

    v1.5.1
    замена получения access token на code по ВК

--- routes/auth/vk.js
+++ routes/auth/vk.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 
+//const
+const CLIENT_SECRET = 'U6YVOpNogZN0JZ4EUNp9';
+const CLIENT_ID = 7865550;
+const REDIRECT_URI = 'https://beatme.online';
+
 //secret
 const { TokenSecret } = require('../../config/keys');
 
@@ -29,29 +34,38 @@ function generateRefreshToken(id, roles){
 
 router.post('/vk', (req, res) => {
     try {
-        const { token } = req.body;
-        axios.get(`https://api.vk.com/method/users.get?access_token=${token}&v=5.131`)
-            .then(async function (response){
-                console.log("response: ", response.data);
-                const { id } = response.data;
-                console.log("id: ", id);
-                const user = await User.findOne({'socialAccount.identity': id});
-                if (user) {
-                    const accessToken = generateAccessToken(user._id, user.roles);
-                    const refreshToken = generateRefreshToken(user._id, user.roles);
-                    const newToken = new Token({token: refreshToken});
-                    await newToken.save();
-                    return res.json({accessToken: accessToken, refreshToken: refreshToken});
-                }
-                else {
-                    const userRole = await Role.findOne({value: "USER"});
-                    const newUser = new User({ username: id, socialAccount: { type: "vk", identity: id}, roles:[userRole.value]});
-                    await newUser.save();
-                    return res.json({ email: id });
-                }
+        const { code } = req.body;
+        axios.get(`https://oauth.vk.com/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&code=${code}`)
+            .then(function (response){
+                console.log("ЗАПРОС: ", `https://oauth.vk.com/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}&code=${code}`);
+                console.log("ACCESS TOKEN response: ", response.data);
+                const { token } = response.data;
+                axios.get(`https://api.vk.com/method/users.get?access_token=${token}&v=5.131`)
+                    .then(async function (response){
+                        console.log("USERID response: ", response.data);
+                        const { id } = response.data;
+                        console.log("USERID id: ", id);
+                        const user = await User.findOne({'socialAccount.identity': id});
+                        if (user) {
+                            const accessToken = generateAccessToken(user._id, user.roles);
+                            const refreshToken = generateRefreshToken(user._id, user.roles);
+                            const newToken = new Token({token: refreshToken});
+                            await newToken.save();
+                            return res.json({accessToken: accessToken, refreshToken: refreshToken});
+                        }
+                        else {
+                            const userRole = await Role.findOne({value: "USER"});
+                            const newUser = new User({ username: id, socialAccount: { type: "vk", identity: id}, roles:[userRole.value]});
+                            await newUser.save();
+                            return res.json({ id: id });
+                        }
+                    }.bind(res))
+                    .catch((err) => {
+                        return res.json({ message: "Не удалось зарегестрировать пользователя через ВК"});
+                    })
             }.bind(res))
             .catch((err) => {
-                return res.json({ message: "Не удалось зарегестрировать пользователя через ВК"});
+                return res.json({ message: "Не удалось получить access token у ВК"});
             })
     }
     catch (e){
